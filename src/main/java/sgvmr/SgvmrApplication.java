e3d25c7c9babacbda1704ca2fb07bfc52bf671f9
David <david.villada@correo.tdea.edu.co>
2024-07-31 14:46:24 -0500

    Initial commit

---
+++ src/main/java/sgvmr/SgvmrApplication.java
@@ -0,0 +1,13 @@
+package sgvmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sgvmr/SgvmrApplicationTests.java
@@ -0,0 +1,13 @@
+package sgvmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
